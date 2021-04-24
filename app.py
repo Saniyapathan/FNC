0f1c282cedf7bf6e5beece1e7ff575b2ac97d5a6
Saniyapathan <54326148+Saniyapathan@users.noreply.github.com>
2021-04-24 22:05:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -38,4 +38,6 @@ def predict():
     return render_template('t.html', prediction_text='The news is "{}"'.format(pred[0]))
     
 if __name__=="__main__":
-    app.run(debug=False,host='0.0.0.0')
+    port=int(os.environ.get('PORT',5000))
+    app.run(port=port,debug=True,use_reloader=False)
+
